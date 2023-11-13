ef67303f91aa280c187d7d3bf7a0cbf18f872901
poncianodev <lucasbponciano@hotmail.com>
2023-11-13 13:26:37 -0300

    Criação da nav mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,27 @@
+// DURAÇÃO DO VÍDEO HEADER
+
 let heroVideo = document.getElementById('hero-video');
 
 heroVideo.currentTime = 20;
 
 heroVideo.addEventListener('ended', () => {
-    heroVideo.currentTime = 162;
-  });
+  heroVideo.currentTime = 162;
+});
 
 heroVideo.play();
+
+// MENU MOBILE
+
+document.addEventListener('DOMContentLoaded', () => {
+    const menuBtn = document.querySelector('.menu-btn');
+    const mobileMenu = document.querySelector('.mobile-menu');
+    const closeBtn = document.querySelector('.close-btn');
+
+    menuBtn.addEventListener('click', () => {
+      mobileMenu.style.display = 'block';
+    });
+
+    closeBtn.addEventListener('click', () => {
+      mobileMenu.style.display = 'none';
+    });
+  });
