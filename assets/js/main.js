5284c782dae1a1a22a90fe12fc7bc1f167fe0544
poncianodev <lucasbponciano@hotmail.com>
2023-11-13 18:45:49 -0300

    alterações no tempo do vídeo do hero

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,8 +4,10 @@ let heroVideo = document.getElementById('hero-video');
 
 heroVideo.currentTime = 20;
 
-heroVideo.addEventListener('ended', () => {
-  heroVideo.currentTime = 162;
+heroVideo.addEventListener('timeupdate', () => {
+  if (heroVideo.currentTime > 130) {
+    heroVideo.currentTime = 0;
+  }
 });
 
 heroVideo.play();
@@ -13,15 +15,15 @@ heroVideo.play();
 // MENU MOBILE
 
 document.addEventListener('DOMContentLoaded', () => {
-    const menuBtn = document.querySelector('.menu-btn');
-    const mobileMenu = document.querySelector('.mobile-menu');
-    const closeBtn = document.querySelector('.close-btn');
+  const menuBtn = document.querySelector('.menu-btn');
+  const mobileMenu = document.querySelector('.mobile-menu');
+  const closeBtn = document.querySelector('.close-btn');
 
-    menuBtn.addEventListener('click', () => {
-      mobileMenu.style.display = 'block';
-    });
-
-    closeBtn.addEventListener('click', () => {
-      mobileMenu.style.display = 'none';
-    });
+  menuBtn.addEventListener('click', () => {
+    mobileMenu.style.display = 'block';
   });
+
+  closeBtn.addEventListener('click', () => {
+    mobileMenu.style.display = 'none';
+  });
+});
